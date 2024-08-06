b153fbf984f3e6c72eca02833181d0f22c7a116e
brian <brian110326@gmail.com>
2024-08-06 18:45:24 +0900

    coupleDiary

---
+++ coupleDiary/couplediary/src/main/java/com/example/couplediary/CouplediaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.couplediary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouplediaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouplediaryApplication.class, args);
+	}
+
+}
